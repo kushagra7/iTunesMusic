af8c91edd64981e9b7f6b66d4735be938a47b3d4
Kushagra Patel <kushagra.kushagrapatel@gmail.com>
2021-02-17 14:39:16 -0500

    Changed Genre Text Color

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -7,32 +7,38 @@ import TextTruncate from 'react-text-truncate';
 const SongItem = ({ album }) => {
     return (
         <a className='cardClick' href={album.link.attributes.href} target="_blank" >
-        <Card className="box" style={{ width: '12rem' }} border='primary' key={album.id.attributes['im:id']}>
-            <Card.Img className="image" variant="top" src={album['im:image'][2].label} />
-            <Card.Body>
-                <Card.Text className='album'>{ }
-                    <TextTruncate
-                        line={1}
-                        truncateText="…"
-                        text={album['im:name'].label}
-                    />
-                </Card.Text>
+            <Card className="box" style={{ width: '12rem' }} border='primary' key={album.id.attributes['im:id']}>
+                <Card.Img className="image" variant="top" src={album['im:image'][2].label} />
+                <Card.Body>
+                    <Card.Text className='album'>{ }
+                        <TextTruncate
+                            line={1}
+                            truncateText="…"
+                            text={album['im:name'].label}
+                        />
+                    </Card.Text>
 
-                <Card.Text className='artist'>
-                    <TextTruncate
-                        line={1}
-                        truncateText="…"
-                        text={album['im:artist'].label}
-                    />
-                </Card.Text>
-                <p className='price'>Price : {album['im:price'].label}</p>
-                <p className='genre'>Genre : {album.category.attributes.label}</p>
-                <Button className="btn" variant="primary" href={album.link.attributes.href} target="_blank">Album Link</Button>
-            </Card.Body>
-            <Card.Footer className='font-release text-muted'>
-                Release Date : {album['im:releaseDate'].attributes.label}
-            </Card.Footer>
-        </Card >
+                    <Card.Text className='artist'>
+                        <TextTruncate
+                            line={1}
+                            truncateText="…"
+                            text={album['im:artist'].label}
+                        />
+                    </Card.Text>
+                    <p className='price'>Price : {album['im:price'].label}</p>
+                    <Card.Text className='genre'>
+                        <TextTruncate
+                            line={1}
+                            truncateText="…"
+                            text={album.category.attributes.label}
+                        />
+                    </Card.Text>
+                    <Button className="btn" variant="primary" href={album.link.attributes.href} target="_blank">Album Link</Button>
+                </Card.Body>
+                <Card.Footer className='font-release text-muted'>
+                    Release Date : {album['im:releaseDate'].attributes.label}
+                </Card.Footer>
+            </Card >
         </a>
     )
 }
